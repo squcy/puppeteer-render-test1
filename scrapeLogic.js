1e05c06273e4a3514dd66aad7de3036260442308
squcy <59390168+squcy@users.noreply.github.com>
2024-05-29 21:05:43 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,24 +17,28 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://api.vevioz.com/api/button/mp3/4xa9Pc-xic0");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+   // await page.type(".search-box__input", "automate beyond recorder");
 
     // Wait and click on first result
     const searchResultSelector = ".search-box__link";
     await page.waitForSelector(searchResultSelector);
     await page.click(searchResultSelector);
 
+    const searchResultSelector2 = ".search-box__link";
+    await page.waitForSelector(searchResultSelector2);
+    //await page.click(searchResultSelector);
+
     // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+ //   const textSelector = await page.waitForSelector(
+//      "text/Customize and automate"
+//    );
+  //  const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
